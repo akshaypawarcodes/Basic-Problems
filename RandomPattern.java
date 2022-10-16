beeae5bd22ed58954ae918e1ff83f4ddc42c54d8
Akshay Kumar <akshayshiv0987@gmail.com>
2022-10-16 09:47:17 +0530

    Update RandomPattern.java

--- RandomPattern.java
+++ RandomPattern.java
@@ -63,5 +63,21 @@ public class RandomPattern {
             }
             System.out.println();
         }
+        
+        //problem 8(Piramid)
+        int i,j, row=4;
+       for(i=0; i<row; i++){
+
+           for (j=row-i; j>1; j--){
+               System.out.print(" ");
+           }
+           for (j=0; j<=i; j++){
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+        
+        //problem 9(Diamond)
+        
     }
 }
