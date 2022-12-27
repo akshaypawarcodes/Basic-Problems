3f174da70d45d57f2061c9b63aef5df32d904d24
Akshay Kumar <akshayshiv0987@gmail.com>
2022-12-27 21:26:37 +0530

    Add files via upload

---
+++ HollowPattern.java
@@ -0,0 +1,30 @@
+
+public class HollowPattern {
+
+    public static void hollow(int n, int m){
+        int i,j;
+        for(i=0; i<=n; i++){
+            for(j=0; j<=m; j++){
+                if(i == 1||i==n || j == 1||j == m){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int row=6;
+        int column=20;
+        hollow(row,column);
+    }
+}
+//OutPut-
+
+//            ********************
+//            *                  *
+//            *                  *
+//            *                  *
+//            *                  *
+//            ********************
