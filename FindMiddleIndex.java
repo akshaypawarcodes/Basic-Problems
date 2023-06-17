df587d0db70768e9a4dd39e5840c3ac5a463d43b
Akshay Kumar <akshayshiv0987@gmail.com>
2023-06-17 21:06:03 +0530

    Update FindMiddleIndex.java

--- FindMiddleIndex.java
+++ FindMiddleIndex.java
@@ -1,29 +1,11 @@
 public class FindMiddleIndex {
-    public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6};
-        int index=0;
-        if(arr.length == 0||arr.length == 1){
-            System.out.println(index);
-        }
-        while(index!= arr.length){
-            int leftSum=0;
-            int rightSum=0;
-            if(index!=0){
-                for(int i=index-1; i>=0; i--){
-                    leftSum=leftSum+arr[i];
-                }
-            }
-            if(index!= arr.length-1){
-                for(int i=index+1; i<arr.length; i++){
-                    rightSum=rightSum+arr[i];
-                }
-            }
-            if(leftSum == rightSum){
-                System.out.println(index);
-                index++;
-            }
-        }
-
-
+   public static void main(String[] args) {
+        int[] arr={2,3,5,4,6,5};
+        findMiddle(arr);
+    }
+    public static void findMiddle(int[] arr){
+        int middle=arr.length/2;
+        int middle1=arr[middle];
+        System.out.println(middle1);
     }
 }
